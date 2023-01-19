d77ea7a29e2d7c3508347cc010635749ec11a46f
Angelaz9 <64970309+Angelaz9@users.noreply.github.com>
2023-01-19 09:45:32 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,30 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+})
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showLeds(`
+        # # . . .
+        . # . . .
+        . # # # .
+        . # # # .
+        . # . # .
+        `)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showString("HELLO!!!")
 })
